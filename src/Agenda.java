547c0107e39d7c8e77047a7fdd00cf1e1eff6854
dam1 <amustafa@elpuig.xeill.net>
2026-09-17 19:28:08 +0200

    Initial commit

---
+++ src/Agenda.java
@@ -0,0 +1,12 @@
+public class Agenda {
+    public static void main(String[] args) {
+
+        System.out.println("Hellcome to the agenda, please choose an option:");
+        System.out.println("  1. Create a new contact.");
+        System.out.println("  2. Search for exising contacts.");
+        System.out.println("  3. Update an existing contact.");
+        System.out.println("  4. Delete an existing contact.");
+        System.out.println("  5. Exit.");
+
+    }
+}
